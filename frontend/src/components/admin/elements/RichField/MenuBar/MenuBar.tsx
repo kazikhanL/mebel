6c6ea970b3e3c58b6abba944fd1b4130e9a0e741
Gereykhanov Kazikhan <kazikhan.lezg@gmail.com>
2022-10-19 23:16:21 +0300

    update

--- frontend/src/components/admin/elements/RichField/MenuBar/MenuBar.tsx
+++ frontend/src/components/admin/elements/RichField/MenuBar/MenuBar.tsx
@@ -17,25 +17,44 @@ const MenuBar = ({ className = "", editor }: MenuBarProps): JSX.Element => {
     const onBulletList = () => editor.chain().focus().toggleBulletList().run();
     const onOrderedList = () => editor.chain().focus().toggleOrderedList().run();
 
+    const defaultButtonStyle = styles.button;
+    const activeButtonStyle = `${styles.button} ${styles.buttonActive}`;
+
+    const titleStyles2 = editor.isActive("heading", { level: 2 }) ? activeButtonStyle : defaultButtonStyle;
+    const titleStyles3 = editor.isActive("heading", { level: 3 }) ? activeButtonStyle : defaultButtonStyle;
+    const titleStyles4 = editor.isActive("heading", { level: 4 }) ? activeButtonStyle : defaultButtonStyle;
+    const titleStyles5 = editor.isActive("heading", { level: 5 }) ? activeButtonStyle : defaultButtonStyle;
+    const titleStyles6 = editor.isActive("heading", { level: 6 }) ? activeButtonStyle : defaultButtonStyle;
+
+    const pharagraphStyles = editor.isActive("paragraph") ? activeButtonStyle : defaultButtonStyle;
+
+    const boldStyles = editor.isActive("bold") ? activeButtonStyle : defaultButtonStyle;
+    const italicStyles = editor.isActive("italic") ? activeButtonStyle : defaultButtonStyle;
+    const strikeStyles = editor.isActive("strike") ? activeButtonStyle : defaultButtonStyle;
+
+    const bulletListStyles = editor.isActive("bulletList") ? activeButtonStyle : defaultButtonStyle;
+    const orderedListStyles = editor.isActive("orderedList") ? activeButtonStyle : defaultButtonStyle;
+
     return (
         <div className={`${styles.wrapper} ${className}`}>
             <div className={styles.container}>
-                <button className={styles.btn} onClick={onTitleLevel2}>h2</button>
-                <button className={styles.btn} onClick={onTitleLevel3}>h3</button>
-                <button className={styles.btn} onClick={onTitleLevel4}>h4</button>
-                <button className={styles.btn} onClick={onTitleLevel5}>h5</button>
-                <button className={styles.btn} onClick={onTitleLevel6}>h6</button>
+                <button className={titleStyles2} onClick={onTitleLevel2}>h2</button>
+                <button className={titleStyles3} onClick={onTitleLevel3}>h3</button>
+                <button className={titleStyles4} onClick={onTitleLevel4}>h4</button>
+                <button className={titleStyles5} onClick={onTitleLevel5}>h5</button>
+                <button className={titleStyles6} onClick={onTitleLevel6}>h6</button>
 
-                <button className={styles.btn} onClick={onPharagraph}>P</button>
+                <button className={pharagraphStyles} onClick={onPharagraph}>P</button>
             </div>
             <div className={styles.container}>
-                <button className={styles.btn} onClick={onBold}>B</button>
-                <button className={styles.btn} onClick={onItalic}>I</button>
-                <button className={styles.btn} onClick={onStrike}>S</button>
+                <button className={boldStyles} onClick={onBold}>B</button>
+                <button className={italicStyles} onClick={onItalic}>I</button>
+                <button className={strikeStyles} onClick={onStrike}>S</button>
+                <button className={strikeStyles} onClick={onStrike}>U</button>
             </div>
             <div className={styles.container}>
-                <button className={styles.btn} onClick={onBulletList}>ul</button>
-                <button className={styles.btn} onClick={onOrderedList}>ol</button>
+                <button className={bulletListStyles} onClick={onBulletList}>ul</button>
+                <button className={orderedListStyles} onClick={onOrderedList}>ol</button>
             </div>
         </div>
     );
